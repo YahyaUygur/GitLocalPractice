fe8d59da749f44026362a124206dd2ea3ce05995
YahyaUygur <av.yahyauygur@gmail.com>
2023-02-27 18:53:33 +0100

    Test1 Uploaded

--- src/ödev/Test1.java
+++ src/ödev/Test1.java
@@ -1,4 +1,7 @@
 package ödev;
 
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
--- src/ödev/Test6.java
+++ src/ödev/Test6.java
@@ -1,7 +1,10 @@
 package ödev;
 
-public class Test2 {
+public class Test6 {
     public static void main(String[] args) {
+        System.out.println("");
+
+
 
     }
 }
